aa9352e70a5386b449834a00b26f9ab65fd6e667
Tejassonar <sonartejas908@gmail.com>
2021-03-28 21:54:20 +0530

    Added : Percentage of Singlet combination

--- multiCoinFlip.sh
+++ multiCoinFlip.sh
@@ -1,15 +1,26 @@
-#!/bin/bash -x
+#!/bin/bash
 
 IS_HEAD=1
-
-
-A=$((RANDOM%2))
-
-if [ $A -eq $IS_HEAD ]
-	then
-		echo "HEAD"
-   else
-      echo "TAILS"
-fi
-
-
+H=0
+T=0
+declare -A result
+for ((i=1; i<=10; i++))
+do
+	A=$((RANDOM%2))
+	if [ $A -eq $IS_HEAD ]
+		then
+			((H++))
+			result[$i]=H
+   	else
+      	((T++))
+			result[$i]=T
+	fi
+done
+echo Head : $H
+echo Tails : $T
+echo ${result[@]}
+echo ${!result[@]}
+Hpe=$(($H*10))
+Tpe=$(($T*10))
+echo "percentage of Head is $Hpe%"
+echo "percentage of Tails is $Tpe%"
