666a982f411fd1d4f05420423ba354b494be4e3c
Tejassonar <sonartejas908@gmail.com>
2021-03-29 20:58:34 +0530

    Added: Doublet Combination with Percentage

--- multiCoinFlip.sh
+++ multiCoinFlip.sh
@@ -1,26 +1,39 @@
-#!/bin/bash
+#!/bin/bash -x
 
-IS_HEAD=1
-H=0
-T=0
-declare -A result
 for ((i=1; i<=10; i++))
 do
 	A=$((RANDOM%2))
-	if [ $A -eq $IS_HEAD ]
+	B=$((RANDOM%2))
+HH=0
+TT=0
+TH=0
+HT=0
+num=$(($A*10+$B))
+result[$i]=$num
+done
+for ((i=1; i<=10; i++))
+do
+	if [ ${result[$i]} -eq 0 ]
 		then
-			((H++))
-			result[$i]=H
-   	else
-      	((T++))
-			result[$i]=T
+		coinres[$i]=HH
+		((HH++))
+	elif [ ${result[$i]} -eq 1 ]
+		then
+		coinres[$i]=TT
+		((TT++))
+	elif [ ${result[$i]} -eq 10 ]
+		then
+		coinres[$i]=TH
+		((TH++))
+	elif [ ${result[$i]} -eq 11 ]
+		then
+		coinres[$i]=HT
+		((HT++))
 	fi
 done
-echo Head : $H
-echo Tails : $T
-echo ${result[@]}
-echo ${!result[@]}
-Hpe=$(($H*10))
-Tpe=$(($T*10))
-echo "percentage of Head is $Hpe%"
-echo "percentage of Tails is $Tpe%"
+echo ${coinres[@]}
+echo "Percentage of HH = $(($HH*10))%"
+echo "Percentage of TT = $(($TT*10))%"
+echo "Percentage of TH = $(($TH*10))%"
+echo "Percentage of HT = $(($HT*10))%"
+
