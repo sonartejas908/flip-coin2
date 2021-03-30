feddb228857916f691ab72eef2b6b0f065fd7df5
Tejassonar <sonartejas908@gmail.com>
2021-03-30 12:53:32 +0530

    Added : Triplet Flipping Combination

--- multiCoinFlip.sh
+++ multiCoinFlip.sh
@@ -1,39 +1,32 @@
 #!/bin/bash -x
-
+HHH=0
+HHT=0
+HTH=0
+THH=0
+HTT=0
+THT=0
+TTH=0
+TTT=0
 for ((i=1; i<=10; i++))
 do
-	A=$((RANDOM%2))
-	B=$((RANDOM%2))
-HH=0
-TT=0
-TH=0
-HT=0
-num=$(($A*10+$B))
-result[$i]=$num
+	A=$((RANDOM%8+1))
+	case $A in
+		1) result[$i]=HHH HHH=$(($HHH+1)) ;;
+		2) result[$i]=HHT HHT=$(($HHT+1)) ;;
+		3) result[$i]=HTH HTH=$(($HTH+1)) ;;
+		4) result[$i]=THH THH=$(($THH+1)) ;;
+		5) result[$i]=HTT HTT=$(($HTT+1)) ;;
+		6) result[$i]=THT THT=$(($THT+1)) ;;
+		7) result[$i]=TTH TTH=$(($TTH+1)) ;;
+   	8) result[$i]=TTT TTT=$(($TTT+1)) ;;
+	esac
 done
-for ((i=1; i<=10; i++))
-do
-	if [ ${result[$i]} -eq 0 ]
-		then
-		coinres[$i]=HH
-		((HH++))
-	elif [ ${result[$i]} -eq 1 ]
-		then
-		coinres[$i]=TT
-		((TT++))
-	elif [ ${result[$i]} -eq 10 ]
-		then
-		coinres[$i]=TH
-		((TH++))
-	elif [ ${result[$i]} -eq 11 ]
-		then
-		coinres[$i]=HT
-		((HT++))
-	fi
-done
-echo ${coinres[@]}
-echo "Percentage of HH = $(($HH*10))%"
-echo "Percentage of TT = $(($TT*10))%"
-echo "Percentage of TH = $(($TH*10))%"
-echo "Percentage of HT = $(($HT*10))%"
-
+echo ${result[@]}
+echo Percentage of HHH is  $(($HHH*10))%
+echo Percentage of HHT is  $(($HHT*10))%
+echo Percentage of HTH is  $(($HTH*10))%
+echo Percentage of THH is  $(($THH*10))%
+echo Percentage of HTT is  $(($HTT*10))%
+echo Percentage of THT is  $(($THT*10))%
+echo Percentage of TTH is  $(($TTH*10))%
+echo Percentage of TTT is  $(($TTT*10))%
